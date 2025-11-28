66f19435edbd5a5b4933e0dd3b469ec0c2c074c2
hyeri.dev <hyeri.dev@gmail.com>
2025-11-28 14:56:00 +0900

    init time zone

--- src/main/kotlin/com/kkomo/KkomoApplication.kt
+++ src/main/kotlin/com/kkomo/KkomoApplication.kt
@@ -1,10 +1,17 @@
 package com.kkomo
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
-class KkomoApplication
+class KkomoApplication {
+    @PostConstruct
+    fun started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main(args: Array<String>) {
 	runApplication<KkomoApplication>(*args)
