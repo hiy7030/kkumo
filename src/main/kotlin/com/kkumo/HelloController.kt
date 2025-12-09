f23b40b2b40e7c0e369af44a9dc088381bb8c14b
hyeri.dev <hyeri.dev@gmail.com>
2025-12-09 10:35:49 +0900

    fix: test controller

--- src/main/kotlin/com/kkumo/HelloController.kt
+++ src/main/kotlin/com/kkumo/HelloController.kt
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/kkumo/v1")
+//@RequestMapping("/kkumo/v1")
 class HelloController {
     @GetMapping("/hello")
     fun hello(): ResponseEntity<String> {
         return ResponseEntity.ok().body("Hello World")
+    }
+
+    @GetMapping()
+    fun hello2(): ResponseEntity<String> {
+        return ResponseEntity.ok().body("Hello World!!!")
     }
 }
