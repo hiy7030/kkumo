20c9ec22ff299fe7d696ca2a140f92767ffd5479
hyeri.dev <hyeri.dev@gmail.com>
2025-12-15 18:06:05 +0900

    fix: security config

--- src/main/kotlin/com/kkumo/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/kkumo/global/config/SecurityConfig.kt
@@ -62,6 +62,9 @@ class SecurityConfig {
                 authorize("/kkumo/v1/signup", permitAll)
                 authorize("/kkumo/v1/members", permitAll)
 
+                // 이메일 인증 허용 (회원가입 시 필요)
+                authorize("/kkumo/v1/mail/**", permitAll)
+
                 // 테스트 API 허용
                 authorize("/kkumo/v1/hello", permitAll)
 
